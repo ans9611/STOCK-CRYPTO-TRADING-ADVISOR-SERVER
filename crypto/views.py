91f9e0c66d0b2aa1a43938a643be7efd048eea7b
ans9611 <ans9611@gmail.com>
2021-09-20 11:52:56 -0400

    added live price of Crypto

--- crypto/views.py
+++ crypto/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 def home(request):
   import requests
   import json
+
+  price_request = requests.get(
+      "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,DOGE,SHIB,ETH,XRP,SOL&tsyms=USD")
+  price = json.loads(price_request.content)
+
+
   api_request = requests.get(
       "https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
   api = json.loads(api_request.content)
   # api dictionary
-  return render(request, 'home.html', {'api': api})
+  return render(request, 'home.html', {'api': api, 'price': price})
