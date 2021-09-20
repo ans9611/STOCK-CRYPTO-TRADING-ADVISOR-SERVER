1466ff344be8166f345b13a7f88552e94ce74489
ans9611 <ans9611@gmail.com>
2021-09-20 11:31:55 -0400

    added new crypto news features

--- crypto/views.py
+++ crypto/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html', {})
+  import requests
+  import json
+  api_request = requests.get(
+      "https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+  api = json.loads(api_request.content)
+  # api dictionary
+  return render(request, 'home.html', {'api': api})
