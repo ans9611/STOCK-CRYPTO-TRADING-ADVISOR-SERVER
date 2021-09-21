31b6481d32af08ee63a80269327b92cdc78c83bb
ans9611 <ans9611@gmail.com>
2021-09-21 01:00:25 -0400

    added notes and sources for employees

--- crypto/urls.py
+++ crypto/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('add_stock/', views.add_stock, name="add_stock"),
     path('delete/<stock_id>', views.delete, name="delete"),
-    path('delete_stock/', views.delete_stock, name="delete_stock")
+    path('delete_stock/', views.delete_stock, name="delete_stock"),
+    path('sources/', views.sources, name="sources"),
+    path('note/', views.note, name="note"),
 ]
